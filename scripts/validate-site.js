7ea1ea530aae1d20aaaa79964ec1286292348468
Weston Rogers <weston.rogers@ofarrellschool.org>
2026-09-16 14:02:00 -0700

    Add division of powers textbook diagram

--- scripts/validate-site.js
+++ scripts/validate-site.js
@@ -269,7 +269,7 @@ function validateSharedCourseExperience() {
   const homepage = fs.readFileSync(path.join(root, 'index.html'), 'utf8');
   [
     'styles.css?v=20260915-intentional-ticket',
-    'course-data.js?v=20260916-105-federalism',
+    'course-data.js?v=20260916-105-division',
     'foundations-data.js?v=20260909-madison-brutus',
     'data-required.js?v=20260805-foundations-cases',
     'app.js?v=20260915-intentional-ticket',
